01315f676d77a92d2a245eca348715e40bf27aee
Veluu <16919963+Veluu@users.noreply.github.com>
2022-10-02 17:15:45 -0400

    add name

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,6 +8,12 @@ variable "subject" {
    description = "Subject to hello"
 }
 
+variable "name" {
+   type = string
+   default = "value-of-name-variable"
+   description = "Subject to hello"
+}
+
 output "hello_world" {
-  value = "hey hey ya, ${var.subject}!"
+  value = "hey hey ya, ${var.subject} and ${var.name}!"
 }
